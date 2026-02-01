bb60275ca02cdc1f7fa395a1f9541efc679fb1a2
JordanSkousen <jotheman21@gmail.com>
2026-02-01 14:30:07 -0700

    Fix launcher

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 cd /home/jordan/source/repos/sleep-machine
+source .venv/bin/activate
 export ELEVENLABS_API_KEY=$(cat ELEVENLABS_API_KEY) EIGHTSLEEP_USERNAME=$(cat EIGHTSLEEP_USERNAME) EIGHTSLEEP_PASSWORD=$(cat EIGHTSLEEP_PASSWORD) GEMINI_API_KEY=$(cat GEMINI_API_KEY)
-python main.py
+python main.py &> cron.log
